2db563d21094e9684600e43d1adcb91b89b1eac9
Asif Mahmud <asifmahmud802@gmail.com>
2024-07-29 23:53:02 +0600

    Responsive done

---
+++ script.js
@@ -0,0 +1,25 @@
+// nav hamburg menu
+// const menuIcon = document.getElementById('menu-icon');
+//     const navbar = document.getElementById('navbar');
+
+//     menuIcon.addEventListener('click', () => {
+//         navbar.classList.toggle('active');
+//     });
+
+
+// home
+
+// var typed = new Typed(".multiple-text", {
+//     strings: ["Web Developer", "Frontend Developer", "Backend Developer", "MERN Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
+
+// const menuIcon = document.getElementById('menu-icon');
+// const navbar = document.getElementById('navbar');
+
+// menuIcon.addEventListener('click', () => {
+//     navbar.classList.toggle('active');
+// });
